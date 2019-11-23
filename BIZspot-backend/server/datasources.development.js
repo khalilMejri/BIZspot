108aad21218b54209a8b889d663b67e5c162990a
topbiz0 <topbiz800@gmail.com>
2019-11-23 17:06:14 +0100

    added db config

---
+++ BIZspot-backend/server/datasources.development.js
@@ -0,0 +1,7 @@
+require('dotenv').config({ path: __dirname + '/./../.env' });
+module.exports = {
+    atlasMongo: {
+        connector: 'mongodb',
+        url: process.env.MONGODB_URL_DEV
+    }
+}
