f77e20f9ca7179477f3b66766841bb848f986fa3
khalil.M <khalil.0927mejri@gmail.com>
2019-12-16 20:50:57 +0100

    added remote hook

--- BIZspot-backend/common/models/review.js
+++ BIZspot-backend/common/models/review.js
@@ -1,5 +1,13 @@
 'use strict';
 
-module.exports = function(Review) {
+module.exports = function (Review) {
 
+    var app = require("../../server/server")
+    Review.afterRemote('create', function (ctx, review, next) {
+        console.log('review has been saved', review);
+        var Business = app.models.Business;
+
+
+        next();
+    });
 };
