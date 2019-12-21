f9ee5db9053c2dee87f1878dcfb13556bacbfbe6
khalil.M <khalil.0927mejri@gmail.com>
2019-12-21 01:55:44 +0100

    serve static angular build

--- source/server/boot/root.js
+++ source/server/boot/root.js
@@ -5,9 +5,9 @@
 
 'use strict';
 
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
+module.exports = function (server) {
+  // // Install a `/` route that returns server status
+  // var router = server.loopback.Router();
+  // router.get('/', server.loopback.status());
+  // server.use(router);
 };
